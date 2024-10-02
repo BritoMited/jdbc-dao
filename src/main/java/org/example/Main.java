89bf7722d9a38e13d38247c322b2a6c95fbf38b9
BritoMited <gustavo.ponciano2005@outlook.com>
2024-10-02 09:33:19 -0300

    Aula "SELECT *"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import org.example.db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try{
+
+            conn = DB.getConnection();
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM department");
+
+            //Integer soma = 0;
+
+            while (resultSet.next()){
+
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+
+             //   soma += resultSet.getInt("Id");
+
+            }
+
+            //System.out.println(soma);
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+
+        }finally{
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
+
     }
 }
