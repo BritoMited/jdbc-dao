1d748f1bfae58e677c1d12b3da31595392a1a762
BritoMited <gustavo.ponciano2005@outlook.com>
2024-11-24 21:10:18 -0300

    update e delete

---
+++ src/main/java/org/example/MainDelete.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.example.db.DB;
+import org.example.db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MainDelete {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                   "DELETE FROM department " +
+                           "WHERE " +
+                           "Id = ?");
+
+            st.setInt(1,2);
+
+            int linhasAfetadas = st.executeUpdate();
+
+            System.out.println(linhasAfetadas);
+        }catch (SQLException e){
+            throw new DbIntegrityException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
---
+++ src/main/java/org/example/MainUpdate.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.example.db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MainUpdate {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "UPDATE seller " +
+                            "SET BaseSalary = BaseSalary + ? " +
+                            "WHERE " +
+                            "(DepartmentId = ?)"
+            );
+
+            st.setDouble(1, 200);
+            st.setInt(2, 2);
+
+            int linhasAfetadas = st.executeUpdate();
+
+            System.out.println(linhasAfetadas);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
